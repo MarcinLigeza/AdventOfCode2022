670a983590f30506f45c8404511e27d02855ed45
MarcinLigeza <marcin.ligeza2@gmail.com>
2022-12-09 21:15:53 +0100

    third day

--- 0_3/main.cpp
+++ 0_3/main.cpp
@@ -9,28 +9,65 @@ char find_first_common_char(const std::string & s1, const std::string & s2)
 {
     for(const auto & c1: s1)
     {
-        for(const auto)
+        for(const auto & c2 : s2)
+        {
+            if (c1 == c2)
+            {
+                return c1;
+            }
+        }
     }
+    return 'a';
+}
+
+uint64_t first_stage(vector<string> data)
+{
+    uint64_t sum {0};
+
+    for(const auto & line : data)
+    {
+        auto first_half = line.substr(0, line.length()/2);
+        auto second_half = line.substr(line.length()/2);
+       
+        auto c = find_first_common_char(first_half, second_half);
+
+        sum += (std::isupper(c) ? (c - 'A' + 26 + 1) : (c - 'a' + 1));
+    }
+    return sum;
+}
+
+char find_common_char(const std::string & s1,
+                      const std::string & s2,
+                      const std::string & s3)
+{
+    for(const auto & c1: s1)
+    {
+        for(const auto & c2 : s2)
+        {
+            for(const auto & c3 : s3)
+            {
+                if (c1 == c2 && c2 == c3)
+                {
+                    return c1;
+                }
+            }
+        }
+    }
+    return 'a';    
 }
 
 int main()
 {
     auto data = load_data("data.txt");
 
-    for(const auto & line : data)
+    uint64_t sum {0};
+
+    for (auto it = data.begin(); it < data.end(); it += 3)
     {
-        auto first_half = line.substr(0, line.length()/2);
-        auto second_half = line.substr(line.length()/2);
+        auto c = find_common_char(*it, *(it+1), *(it+2));
 
-        std::sort(first_half.begin(), first_half.end());
-        std::sort(second_half.begin(), second_half.end());
-
-        std::string result;
-        std::set_intersection(first_half.begin(), first_half.end(),
-                                second_half.begin(), second_half.end(),
-                                std::back_inserter(result));
-
-        std::cout << first_half << " " << second_half << " " <<  result << endl;
+        sum += (std::isupper(c) ? (c - 'A' + 26 + 1) : (c - 'a' + 1));        
     }
 
+    cout << sum << endl;
 }
